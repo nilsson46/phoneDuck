e45fd37446f71517f81e15d459294907b84720e5
nilsson46 <nilssonsimon46@gmail.com>
2023-02-08 11:17:10 +0100

    Initial commit

---
+++ src/main/java/com/example/phoneduck/PhoneDuckApplication.java
@@ -0,0 +1,13 @@
+package com.example.phoneduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneDuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneDuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phoneduck/PhoneDuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phoneduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneDuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
