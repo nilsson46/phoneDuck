9227de59eca355b6e858592525b228e190089552
nilsson46 <nilssonsimon46@gmail.com>
2023-02-10 11:51:18 +0100

    Added a method to check if the channel exists

--- src/main/java/com/example/phoneduck/wesocket/ChatSocketHandler.java
+++ src/main/java/com/example/phoneduck/wesocket/ChatSocketHandler.java
@@ -1,6 +1,8 @@
 package com.example.phoneduck.wesocket;
 
+import com.example.phoneduck.model.Channel;
 import com.example.phoneduck.service.ChannelService;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -17,31 +19,60 @@ public class ChatSocketHandler extends TextWebSocketHandler {
     @Autowired
     ChannelService channelService;
 
+    private List<Channel> channelList;
     private List<WebSocketSession> sessions = new ArrayList<>();
 
-    public void broadcast(String channel, String message) {
+
+    public void broadcast(String message) {
         try{
-            for(WebSocketSession webSocketSession : sessions){
-                String room = webSocketSession.getHandshakeHeaders().getFirst("room");
-                if(channel.equals(room)){
+            for(WebSocketSession webSocketSession : sessions) {
                     webSocketSession.sendMessage(new TextMessage("Message: " + message));
                 }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
+   /* public void broadcast(String message) {
+        try{
+            for(WebSocketSession webSocketSession : sessions){
+                String listOfActiveChannels = channelService.getAll().toString();
+                String chatChannel = webSocketSession.getHandshakeHeaders().getFirst("chatchannel");
+                if(listOfActiveChannels.contains(chatChannel)){
+                    webSocketSession.sendMessage(new TextMessage("Message: " + message));
+                }else{
+                    throw new IllegalStateException("This channel is not active");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    } */
+
     @Override
-    protected void handleTextMessage(WebSocketSession webSocketSession, TextMessage textMessage) throws Exception{
-        
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        broadcast(message.getPayload());
     }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception{
-        sessions.add(webSocketSession);
-        System.out.println("New chat session started");
+        String listOfActiveChannels = channelService.getAll().toString();
+        String room = webSocketSession.getHandshakeHeaders().getFirst("room");
+        if(listOfActiveChannels.contains(room)){
+            webSocketSession.sendMessage(new TextMessage("Welcome to the channel, send a message to see if anyone is active"));
+            sessions.add(webSocketSession);
+            System.out.println("New chat session started");
+        }else{
+            throw new IllegalStateException("This channel is not active");
+        }
     }
+
+      /*  sessions.add(webSocketSession);
+        webSocketSession.sendMessage(new TextMessage("Welcome to the channel, send a message to see if anyone is active"));
+        System.out.println("New chat session started");
+    } */
 
     @Override
     public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception{
