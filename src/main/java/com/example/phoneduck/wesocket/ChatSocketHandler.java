efa8e25b79bea82408f4640824b016aa3bc3f273
nilsson46 <nilssonsimon46@gmail.com>
2023-02-13 11:09:29 +0100

    Fixed so you can join more then one channel

--- src/main/java/com/example/phoneduck/wesocket/ChatSocketHandler.java
+++ src/main/java/com/example/phoneduck/wesocket/ChatSocketHandler.java
@@ -1,8 +1,6 @@
 package com.example.phoneduck.wesocket;
 
-import com.example.phoneduck.model.Channel;
 import com.example.phoneduck.service.ChannelService;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -19,7 +17,9 @@ public class ChatSocketHandler extends TextWebSocketHandler {
     @Autowired
     ChannelService channelService;
 
-    private List<Channel> channelList;
+   // private Channel channel;
+
+    //private List<Channel> channelList;
     private List<WebSocketSession> sessions = new ArrayList<>();
 
 
@@ -34,7 +34,39 @@ public class ChatSocketHandler extends TextWebSocketHandler {
 
     }
 
-   /* public void broadcast(String message) {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+       // String channel = session.getHandshakeHeaders().get("room").toString();
+        broadcast(message.getPayload());
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception{
+        String listOfActiveChannels = channelService.getAll().toString();
+        //System.out.println(channelService.getAll().toString());
+        //System.out.println(webSocketSession.getHandshakeHeaders().get("room"));
+        String room = webSocketSession.getHandshakeHeaders().getFirst("room");
+      if(listOfActiveChannels.contains(room)){
+          webSocketSession.sendMessage(new TextMessage("Welcome to the channel " + webSocketSession.getHandshakeHeaders().get("room").toString() + " send a message to see if anyone is active"));
+            sessions.add(webSocketSession);
+            System.out.println("New chat session started");
+     }else{
+         throw new IllegalStateException("This channel is not active");
+      }
+   }
+
+      /*sessions.add(webSocketSession);
+        webSocketSession.sendMessage(new TextMessage("Welcome to the channel, send a message to see if anyone is active"));
+        System.out.println("New chat session started"); */
+
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception{
+        sessions.remove(webSocketSession);
+        System.out.println("Chat session was removed");
+    }
+}
+/* public void broadcast(String message) {
         try{
             for(WebSocketSession webSocketSession : sessions){
                 String listOfActiveChannels = channelService.getAll().toString();
@@ -50,34 +82,4 @@ public class ChatSocketHandler extends TextWebSocketHandler {
         }
 
     } */
-
-    @Override
-    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        broadcast(message.getPayload());
-    }
-
-    @Override
-    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception{
-        String listOfActiveChannels = channelService.getAll().toString();
-        String room = webSocketSession.getHandshakeHeaders().getFirst("room");
-        if(listOfActiveChannels.contains(room)){
-            webSocketSession.sendMessage(new TextMessage("Welcome to the channel, send a message to see if anyone is active"));
-            sessions.add(webSocketSession);
-            System.out.println("New chat session started");
-        }else{
-            throw new IllegalStateException("This channel is not active");
-        }
-    }
-
-      /*  sessions.add(webSocketSession);
-        webSocketSession.sendMessage(new TextMessage("Welcome to the channel, send a message to see if anyone is active"));
-        System.out.println("New chat session started");
-    } */
-
-    @Override
-    public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception{
-        sessions.remove(webSocketSession);
-        System.out.println("Chat session was removed");
-    }
-}
 
